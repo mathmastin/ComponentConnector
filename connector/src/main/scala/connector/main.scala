b5dfcd9d9f6b9093e2aa10d0dea965d21558b90a
Jason Walker <JasonW94@gmail.com>
2015-06-10 13:28:29 -0400

    Removed build stuff & added cc example.

--- connector/src/main/scala/connector/main.scala
+++ connector/src/main/scala/connector/main.scala
@@ -1,5 +1,25 @@
 package connector
 
+import org.apache.spark._
+import org.apache.spark.graphx._
+import org.apache.spark.rdd.RDD
+
 object MyApp {
-       def main(args: Array[String]) = println("HI")
+       def main(args: Array[String]) {
+       	   // Create the spark context
+	   val sc = new SparkContext()
+       	   // Load the graph
+	   val graph = GraphLoader.edgeListFile(sc, "graphx/data/followers.txt")
+	   // Find the cc's
+	   val cc = graph.connectedComponents().vertices
+	   // Join the cc's with usernames
+	   val users = sc.textFile("graphx/data/users.txt").map { line =>
+	       val fields = line.split(",")(fields(0).toLong, fields(1))
+	   }
+	   val ccByUsername = users.join(cc).map {
+	       case (id, (username, cc)) => (username, cc)
+	   }
+	   // Print the result
+	   println(ccByUsername.collect().mkString("\n"))
+       }
 }
