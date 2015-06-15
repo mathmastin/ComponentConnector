ec1b48bb3f383b6020b21427435de6609dcbb3f3
Jason Walker <JasonW94@gmail.com>
2015-06-15 15:16:08 -0400

    Progress on EdgeRDDBuilder and test in main.

--- connector/src/main/scala/connector/controller/edge_rdd_builder.scala
+++ connector/src/main/scala/connector/controller/edge_rdd_builder.scala
@@ -1,6 +1,31 @@
 package connector.controller
 
 import akka.actor.Actor
+import org.apache.spark._
+import org.apache.spark.graphx._
+import org.apache.spark.graphx.impl.GraphImpl
 
-class EdgeRddBuilder {
+sealed abstract trait EdgeRddBuilderMessage
+case object Reset extends EdgeRddBuilderMessage
+case class AddEdge[Int](edge: Edge[Int]) extends EdgeRddBuilderMessage
+case object RequestGraph extends EdgeRddBuilderMessage
+
+class EdgeRddBuilder(sc: SparkContext) extends Actor {
+	private var edges: EdgeRDD[Int] = newEdgeRDD
+
+	def newEdgeRDD() = EdgeRDD.fromEdges(sc.parallelize(Array[Edge[Int]]()))
+
+	def receive = {
+		case Reset => edges = newEdgeRDD
+		case AddEdge(edge) => {
+			//TODO: Find some way to avoid this horrible type casting?
+			val newRdd: EdgeRDD[Int] = EdgeRDD.fromEdges[Int, Int](sc.parallelize(List(edge.asInstanceOf[Edge[Int]])))
+			edges = EdgeRDD.fromEdges(edges.union(newRdd))
+		}
+		case RequestGraph => {
+			val verts = VertexRDD.fromEdges(edges, 1, 0)
+			val graph = GraphImpl.fromExistingRDDs(verts, edges)
+			sender ! SendGraph(graph)
+		}
+	}
 }
--- connector/src/main/scala/connector/main.scala
+++ connector/src/main/scala/connector/main.scala
@@ -4,14 +4,14 @@ import org.apache.spark._
 import org.apache.spark.graphx._
 import org.apache.spark.graphx.impl._
 import org.apache.spark.rdd.RDD
+import akka.actor._
+import connector.controller.Controller
+import connector.controller.ReadFromFile
 
 object MyApp {
   def main(args: Array[String]) {
-    // Create the spark context
-    val conf = new SparkConf()
-      .setMaster("local[2]")
-    GraphXUtils.registerKryoClasses(conf)
-    val sc = new SparkContext(conf.setAppName("TestApp"))
+
+    /*
     // Load the graph
     val graph = GraphLoader.edgeListFile(sc, "data/followers.txt")
     // Find the cc's
@@ -28,5 +28,37 @@ object MyApp {
     println(ccByUsername.collect().mkString("\n"))
     // Stop the context
     sc.stop()
+    */
+
+    /*val emtpyEdges = Array[Edge[Int]](Edge(1,2), Edge(1,3), Edge(3, 4), Edge(6, 7))
+    val emptyRdd = sc.parallelize(emtpyEdges)
+    val edgeRdd = EdgeRDD.fromEdges(emptyRdd)
+    val graph = Graph.fromEdges(edgeRdd, 0)
+    val cc = graph.connectedComponents().vertices
+
+    println(cc.collect().mkString("\n"))
+
+    sc.stop()*/
+    
+    /// Set up spark ///
+	// Create the spark context
+    val conf = new SparkConf()
+      .setMaster("local[2]")
+    GraphXUtils.registerKryoClasses(conf)
+    val sc = new SparkContext(conf.setAppName("TestApp"))
+
+    /// Set up Akka ///
+    val system = ActorSystem("Edge-Test")
+    
+    /// Start the problem ///
+    val dataFile = "data/followers.txt"
+    
+   	val controller = system.actorOf(Props(new Controller(system, sc)), "controller")   
+   	
+   	controller ! ReadFromFile(dataFile)
+   	
+   	Thread.sleep(5000)
+   	sc.stop()
+   	system.shutdown()
   }
 }
