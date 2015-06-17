6da576f7616655ad973611e5db079b396c8240a9
Jason Walker <JasonW94@gmail.com>
2015-06-17 01:47:10 -0400

    Added some ScalaDoc to Controller & EdgeRddBuilder, and cleaned them up a bit.

--- connector/src/main/scala/connector/controller/controller.scala
+++ connector/src/main/scala/connector/controller/controller.scala
@@ -8,16 +8,33 @@ import org.apache.spark._
 import scala.io.Source
 
 sealed abstract trait ControllerMessage 
-case class StartSparkStream(host: String)
+case class StartSparkStream(host: String) extends ControllerMessage
 case class ReadFromFile(fname: String) extends ControllerMessage
 case class SendGraph(g: Graph[Int, Int]) extends ControllerMessage
 case object PrintComps extends ControllerMessage
 
+/** An Akka actor that communicates with the Python side of the framework and directs
+ *  the GraphX calculations on the Scala side. Uses a TCP/IP socket to communicate with
+ *  Python and the Akka Actor system to communicate with other Scala objects.
+ *  Receives messages that inherit from [[connector.controller.ControllerMessage]].
+ *  
+ *  @constructor Use a given ActorSystem & SparkContext.
+ *  @param actSys The actor system
+ *  @param sc The spark context 
+ */
 class Controller(actSys: ActorSystem, sc: SparkContext) extends Actor {
-	var graph: Option[Graph[Int, Int]] = None
-	var streamContext: Option[StreamingContext] = None
-	var edgeStream: Option[DStream[String]] = None
+	private var graph: Option[Graph[Int, Int]] = None
+	//TODO: Use TCP/IP socket instead of spark streaming.
+	private var streamContext: Option[StreamingContext] = None
+	private var edgeStream: Option[DStream[String]] = None
 	
+	/*** Implement the messages ***/
+	private def startSparkStream(host: String) {
+		streamContext = Some(new StreamingContext(sc, Seconds(1)))
+		edgeStream = Some(streamContext.get.socketTextStream(host, 9999))
+		
+		val edgeBuilder = actSys.actorOf(Props(new EdgeRddBuilder(sc)), "edgebuilder")
+	}
 	private def buildGraphFromFile(fname: String) {
 		val edgeBuilder = actSys.actorOf(Props(new EdgeRddBuilder(sc)), "edgebuilder")
 		for(lines <- Source.fromFile(fname).getLines()) {
@@ -27,22 +44,18 @@ class Controller(actSys: ActorSystem, sc: SparkContext) extends Actor {
 		}
 		edgeBuilder ! RequestGraph
 	}
+	private def sendGraph(g: Graph[Int, Int]) {
+		graph = Some(g); self ! PrintComps //TODO: This is a bit hack-y
+	}
+	private def printComps {
+		val cc = graph.get.connectedComponents().vertices
+		println(cc.collect().mkString("\n"))
+	}
 	
 	def receive = {
-		case StartSparkStream(host) => {
-			streamContext = Some(new StreamingContext(sc, Seconds(1)))
-			edgeStream = Some(streamContext.get.socketTextStream(host, 9999))
-			
-			val edgeBuilder = actSys.actorOf(Props(new EdgeRddBuilder(sc)), "edgebuilder")
-			
-			//edgeStream.flatMap(_.split(" "))
-
-		}
+		case StartSparkStream(host) => startSparkStream(host)
 		case ReadFromFile(fname) => buildGraphFromFile(fname)
-		case SendGraph(g) => graph = Some(g); self ! PrintComps //TODO: This is a bit hack-y
-		case PrintComps => {
-			val cc = graph.get.connectedComponents().vertices
-			println(cc.collect().mkString("\n"))
-		}
+		case SendGraph(g) => sendGraph(g)
+		case PrintComps => printComps
 	}
 }
--- connector/src/main/scala/connector/controller/edge_rdd_builder.scala
+++ connector/src/main/scala/connector/controller/edge_rdd_builder.scala
@@ -1,6 +1,6 @@
 package connector.controller
 
-import akka.actor.Actor
+import akka.actor.{Actor, ActorRef}
 import org.apache.spark._
 import org.apache.spark.graphx._
 import org.apache.spark.graphx.impl.GraphImpl
@@ -10,22 +10,37 @@ case object Reset extends EdgeRddBuilderMessage
 case class AddEdge[Int](edge: Edge[Int]) extends EdgeRddBuilderMessage
 case object RequestGraph extends EdgeRddBuilderMessage
 
+/** An Akka actor that builds an EdgeRDD from edge objects.
+ *  Should be instantiated using an ActorSystem, not by calling a constructor.
+ *  Receives messages that inherit from [[connector.controller.EdgeRddBuilderMessage]].
+ *  
+ *  @constructor Build the RDD in a specific SparkContext.
+ *  @param sc The spark context 
+ */
 class EdgeRddBuilder(sc: SparkContext) extends Actor {
 	private var edges: EdgeRDD[Int] = newEdgeRDD
 
-	def newEdgeRDD() = EdgeRDD.fromEdges(sc.parallelize(Array[Edge[Int]]()))
+	/** Constructs an empty EdgeRDD. */
+	private def newEdgeRDD() = EdgeRDD.fromEdges(sc.parallelize(Array[Edge[Int]]()))
+	
+	/*** Implement the messages ***/
+	private def reset() { 
+		edges = newEdgeRDD 
+	}
+	private def addEdge(edge: Edge[Any]) {
+		//TODO: Find some way to avoid this horrible type casting?
+		val newRdd: EdgeRDD[Int] = EdgeRDD.fromEdges[Int, Int](sc.parallelize(List(edge.asInstanceOf[Edge[Int]])))
+		edges = EdgeRDD.fromEdges(edges.union(newRdd))
+	}
+	private def requestGraph(sender: ActorRef) {
+		val verts = VertexRDD.fromEdges(edges, 1, 0)
+		val graph = GraphImpl.fromExistingRDDs(verts, edges)
+		sender ! SendGraph(graph)
+	}
 
 	def receive = {
 		case Reset => edges = newEdgeRDD
-		case AddEdge(edge) => {
-			//TODO: Find some way to avoid this horrible type casting?
-			val newRdd: EdgeRDD[Int] = EdgeRDD.fromEdges[Int, Int](sc.parallelize(List(edge.asInstanceOf[Edge[Int]])))
-			edges = EdgeRDD.fromEdges(edges.union(newRdd))
-		}
-		case RequestGraph => {
-			val verts = VertexRDD.fromEdges(edges, 1, 0)
-			val graph = GraphImpl.fromExistingRDDs(verts, edges)
-			sender ! SendGraph(graph)
-		}
+		case AddEdge(edge) => addEdge(edge)
+		case RequestGraph => requestGraph(sender)
 	}
 }
